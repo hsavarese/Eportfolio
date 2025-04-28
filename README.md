41c7df99ff2b44e6f8c7fd6e663ae1737a11d10b
hsavarese <huntersavarese135@gmail.com>
2025-04-28 19:40:20 -0400

    some bug fixs in the js and html

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Hunter Savarese's EPortfolio/Resume
 
 Made for a class assignment, will continue to update it, clean it, work on it as time goes on
+
+
--- hsavarese_portfolio/scripts/skills.js
+++ hsavarese_portfolio/scripts/skills.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var toggles = document.querySelectorAll(".toggle-btn");
+    const toggles = document.querySelectorAll(".toggle-btn");
     toggles.forEach(function (btn) {
       btn.addEventListener("click", function () {
-        var targetId = btn.dataset.target;
-        var target = document.getElementById(targetId);
+        const targetId = btn.dataset.target;
+        const target = document.getElementById(targetId);
         if (target) {
           target.classList.toggle("hidden");
         }
